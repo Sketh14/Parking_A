877fcfeab444a9c31c72162e3e44365dc99cab4c
Avinash Yadav <avinashy1993@gmail.com>
2026-01-01 14:07:28 +0530

    BuildingSpawn

---
+++ Assets/Script/Gameplay/BuildingSpawnner.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSpawnner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _buildingPrefab;
+    [SerializeField] private Transform[] _transformPosition;
+    void Start()
+    {
+
+    }
+
+
+}
