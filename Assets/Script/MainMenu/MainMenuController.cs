5bbee786f11a7855dbc6e4ee4c962926002c3bd6
Avinash Yadav <avinashy1993@gmail.com>
2026-01-01 10:46:06 +0530

    Adding New Cars and Map

--- Assets/Script/MainMenu/MainMenuController.cs
+++ Assets/Script/MainMenu/MainMenuController.cs
@@ -4,19 +4,21 @@ using UnityEngine.UI;
 
 using SceneManager = UnityEngine.SceneManagement.SceneManager;
 
+// tO DO sETTING pANEL AND mUSIC 
 namespace Parking_A.MainMenu
 {
     public enum MainMenuUIStatus { MAIN_MENU, SHOP }
 
     public class MainMenuController : MonoBehaviour
     {
-        [SerializeField] private Button _playBt;// _quitBt;
-        // [SerializeField] private Button _quitYesBt, _quitNoBt;
+        [SerializeField] private Button _playBt, _quitBt;
+        [SerializeField] private Button _quitYesBt, _quitNoBt;
         [SerializeField] private Button _openShopBt;
+        [SerializeField] private Button _rateUsBtn;
 
         [SerializeField] private Toggle _toggleRandomizeLevel;
 
-        // [SerializeField] private GameObject _quitConfirmationPanel;
+        [SerializeField] private GameObject _quitConfirmationPanel;
         [SerializeField] private GameConfigScriptableObject _mainGameConfig;
 
         [SerializeField] private GameObject _mainMenuCanvas;
@@ -31,25 +33,29 @@ namespace Parking_A.MainMenu
         {
             //Assigning Buttons
             _playBt.onClick.AddListener(() => Playlevel());
-            //_quitBt.onClick.AddListener(() => _quitConfirmationPanel.SetActive(true));
+            _quitBt.onClick.AddListener(() => _quitConfirmationPanel.SetActive(true));
 
-            //_quitYesBt.onClick.AddListener(Application.Quit);
-            // _quitNoBt.onClick.AddListener(() => _quitConfirmationPanel.SetActive(false));
+            _quitYesBt.onClick.AddListener(Application.Quit);
+            _quitNoBt.onClick.AddListener(() => _quitConfirmationPanel.SetActive(false));
+
+            // Forced Randomization of the level toggleValue 
 
             _toggleRandomizeLevel.onValueChanged.AddListener((toggleValue) =>
             {
+                toggleValue = true;
                 _mainGameConfig.RandomizeLevel = toggleValue;
                 // Debug.Log("Random: " + System.DateTime.Now.Day.ToString() + System.DateTime.Now.Hour.ToString()
                 //     + System.DateTime.Now.Minute.ToString() + System.DateTime.Now.Second.ToString());
             });
 
-            _mainGameConfig.RandomizeLevel = false;
+            _mainGameConfig.RandomizeLevel = true;
 
             _openShopBt.onClick.AddListener(() =>
             {
                 _mainMenuCanvas.SetActive(false);
                 MainMenuManager.Instance.OnUIInteraction?.Invoke(MainMenuUIStatus.SHOP);
             });
+            _rateUsBtn.onClick.AddListener(() => Application.OpenURL("market://details?id=" + Application.productName));
 
             MainMenuManager.Instance.OnUIInteraction += HandleUIChange;
         }
@@ -71,4 +77,6 @@ namespace Parking_A.MainMenu
         }
 
     }
+
+
 }
